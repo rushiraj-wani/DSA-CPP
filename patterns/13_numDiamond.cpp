8b95f8d0ff274d1dacccfd623f55f08840e44eba
Rushiraj Wani <rajwani2003@gmail.com>
2024-06-12 09:35:42 +0530

    numeric diamond pattern

---
+++ patterns/13_numDiamond.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+/* n = 5
+        1      -> 1
+       1 2     -> 2
+      1 2 3    -> 3
+     1 2 3 4   -> 4
+    1 2 3 4 5  -> 5
+     1 2 3 4   -> 1
+      1 2 3    -> 2
+       1 2     -> 3
+        1      -> 4
+*/
+
+int main(){
+    int n;
+    cout<<"Ente value of n: ";
+    cin >> n;
+
+    for(int row = 1; row <= n; row++){
+        //space
+        for(int col = 1; col <= n-row; col++){
+            cout<<" ";
+        }
+        //num
+        for(int col = 1; col <= row; col++){
+            cout<<col<<" ";
+        }
+        cout<<endl;
+    }
+    for(int row = 1; row <= n; row++){
+        for(int col = 1; col <= row; col++){
+            cout<<" ";
+        }
+        for(int col = 1; col <= n-row; col++){
+            cout<<col<<" ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
